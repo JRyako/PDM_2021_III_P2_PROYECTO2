f3ba84c65c00d73fb2fb596a9d0681162c4a7bc3
Hepburn <91041472+Hepburn23@users.noreply.github.com>
2021-11-16 16:13:31 -0600

    joohny arregla tus cosas fok

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p2_proyecto2/ClientesActivity.kt
@@ -0,0 +1,10 @@
+package hn.edu.ujcv.pdm_2021_iii_p2_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClientesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p2_proyecto2/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p2_proyecto2/MainActivity.kt
@@ -1,11 +1,47 @@
 package hn.edu.ujcv.pdm_2021_iii_p2_proyecto2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hn.edu.ujcv.pdm_2021_iii_p2_proyecto2.Clases.Clientes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_Clientes.setOnClickListener { clientes() }
+        btn_empleados.setOnClickListener { empleados() }
+        btn_facturas.setOnClickListener { facturas() }
+        btn_pedidos.setOnClickListener { pedidos() }
+        btn_menus.setOnClickListener { menus() }
     }
+    private fun inicializar(){
+
+    }
+
+    private fun clientes(){
+        val intent = Intent(this, ClientesActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    private fun empleados(){
+
+
+    }
+    private fun facturas(){
+
+
+    }
+    private fun pedidos(){
+
+
+    }
+    private fun menus(){
+
+
+    }
+
 }
